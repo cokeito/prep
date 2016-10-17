bab7d6d99966f591e67a25eaebb37c98a9db0348
cokeito <coke@mta.cl>
2016-10-17 20:40:57 -0300

    model user created

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
 class PagesController < ApplicationController
   def x
   end
+
+  def save_data
+  	@name = params[:name]
+  	@email = params[:email]
+  	@age = params[:age]
+
+  	x = User.new( name: params[:name], email: @email, age: @age)
+  	x.save
+
+  	redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'pages/x'
+  get 'pages/save_data' 
 
   root 'pages#x'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
